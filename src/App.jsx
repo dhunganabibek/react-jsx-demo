e40c8e0e5bad758287a11503774c03b805c28d1a
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-09-24 01:11:48 -0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  console.log(`isOpen`, isOpen);
+  return (
+    <>
+      <h1> I am feeling lucky</h1>
+      <div style={{ display: "flex" }}>
+        {isOpen && <h1>Hello</h1>}
+        <button onClick={() => setIsOpen(!isOpen)}>X</button>
+      </div>
+
+      <h1> Thank you</h1>
+    </>
+  );
+}
+
+export default App;
